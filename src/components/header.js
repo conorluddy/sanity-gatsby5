732e28d224209a4f5e595825a5211fe7d9bf4e9e
Conor Luddy <conorluddy@gmail.com>
2022-10-31 18:18:34 +0000

    Webhook trigger

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,22 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import {Link} from 'gatsby';
 
 const Header = ({children, size = 'medium'}) => {
   let header = (
     <Link className="header-link-home" to="/">
-      {children}
+      {children ?? 'Home'}
     </Link>
-  )
+  );
 
-  if (size === "large") {
+  if (size === 'large') {
     header = (
       <h1 className="main-heading">
         <Link to="/">{children}</Link>
       </h1>
-    )
+    );
   }
 
-  return (
-    <header className="global-header">
-      {header}
-    </header>
-  )
-}
+  return <header className="global-header">{header}</header>;
+};
 
-export default Header
+export default Header;
