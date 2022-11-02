696430ca98280e285ca0fd6da38c74befc7d889b
Conor Luddy <conorluddy@gmail.com>
2022-11-02 21:25:21 +0000

    Navigation from Sanity pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,44 @@
-import * as React from "react"
-import { Slice } from "gatsby"
+import * as React from 'react';
+import {Slice, useStaticQuery, graphql} from 'gatsby';
+import {Link} from 'gatsby';
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
+const Layout = ({location, title, children}) => {
+  const rootPath = `${__PATH_PREFIX__}/`;
+  const isRootPath = location.pathname === rootPath;
+
+  const data = useStaticQuery(graphql`
+    query PageQuery {
+      allSanityPage {
+        nodes {
+          slug {
+            current
+          }
+          title
+        }
+      }
+    }
+  `);
+
+  const pages = data.allSanityPage.nodes;
+
+  console.log('pages', pages);
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <Slice alias="header" size={isRootPath ? 'large': 'medium'}>{title}</Slice>
+      <Slice alias="header" size={isRootPath ? 'large' : 'medium'}>
+        {title}
+      </Slice>
       <main>{children}</main>
+      <ul>
+        {pages?.map(page => (
+          <li key={page.title}>
+            <Link to={'/' + page.slug.current}>{page?.title}</Link>
+          </li>
+        ))}
+      </ul>
       <Slice alias="footer" />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,32 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import * as React from 'react';
+import {Link, graphql} from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+const BlogIndex = ({data, location}) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`;
+  const posts = data.allMarkdownRemark.nodes;
 
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the
+          "gatsby-source-filesystem" plugin in gatsby-config.js).
         </p>
       </Layout>
-    )
+    );
   }
 
   return (
     <Layout location={location} title={siteTitle}>
-      <ol style={{ listStyle: `none` }}>
+      <ol style={{listStyle: `none`}}>
         {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+          const title = post.frontmatter.title || post.fields.slug;
 
           return (
             <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
+              <article className="post-list-item" itemScope itemType="http://schema.org/Article">
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
@@ -43,23 +38,23 @@ const BlogIndex = ({ data, location }) => {
                 <section>
                   <p
                     dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
+                      __html: post.frontmatter.description || post.excerpt
                     }}
                     itemProp="description"
                   />
                 </section>
               </article>
             </li>
-          )
+          );
         })}
       </ol>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogIndex
+export default BlogIndex;
 
-export const Head = () => <Seo title="All posts" />
+export const Head = () => <Seo title="All posts" />;
 
 export const pageQuery = graphql`
   query {
@@ -68,7 +63,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+    allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
       nodes {
         excerpt
         fields {
@@ -82,4 +77,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
